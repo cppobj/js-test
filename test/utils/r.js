4f1521c141fccd9ad0b6627ea098229544595e10
Denis Bobrov <denis.bobrov.j@gmail.com>
2016-05-23 00:13:33 +0300

    Added intitial implementation of R.bind function.

---
+++ test/utils/r.js
@@ -0,0 +1,11 @@
+import { assert } from 'chai';
+
+import R from '../../src/utils/r';
+
+describe('R', () => {
+  describe('#bind', () => {
+    it('should make partial function with fixed set of args', () => {
+      
+    });
+  });
+});
